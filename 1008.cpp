99580b9442d6e0e2711c4fd5e78c400647ff9197
Eason20120229 <181348642@qq.com>
2024-01-29 18:29:56 +0800

    ff

---
+++ 1008.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ans;
+int a[10];
+int book[10];
+
+int shu(int i){
+    return a[i] * 100 + a[i + 1] * 10 + a[i + 2];
+}
+
+void dfs(int n){
+    if(n == 10){
+        if(shu(1) == shu(4) / 2 && shu(7) / 3 == shu(1)){
+            cout << shu(1) << " " << shu(4) << " " << shu(7) << endl;
+        }
+        return;
+    }
+    for(int i = 1;i <= 9;i++){
+        if(!book[i]){
+            book[i] = 1;
+            a[n] = i;
+            dfs(n + 1);
+            book[i] = 0;
+        }
+    }
+}
+
+int main(){
+    dfs(1);
+    return 0;
+}
---
+++ 1047.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[10001];
+
+int main(){
+    int l,m;
+    cin >> l >> m;
+    while(m--){
+        int u,v;
+        cin >> u >> v;
+        for(int i = u;i <= v;i++){
+            a[i] = 1;
+        }
+    }
+    int ans = 0;
+    for(int i = 0;i <= l;i++){
+        if(!a[i]) ans++;
+    }
+    cout << ans;
+    return 0;
+}
