b6bc9cc32a0d8ae26291d88542efd22414acf23f
Eason20120229 <181348642@qq.com>
2024-07-30 15:06:49 +0800

    two files

--- 2725.cpp
+++ 2725.cpp
@@ -1,18 +1,30 @@
 /*
 
-//1 2 1+2 4 1+4 2+4 1+2+4 1+1+2+4 1+1+1+2+4 1+1+2+2+4 1+2+2+2+4 2+2+2+2+4
+//1 2 1+2 4 1+4 2+4 1+2+4 4+4 1+1+1+2+4 1+1+2+2+4 1+2+2+2+4 2+2+2+2+4
 //4+4+4+1 4+4+4+1+1 4+4+4+1+2 4+4+4+4 4+4+4+4+1 4+4+4+4+2
 5 3
 1 2 4
 :18
 
+10 3
+1 2 3
+
+
 */
 
+#include <algorithm>
 #include <iostream>
 #define N 201
+#define M 2000001
 
-bool dparr[N][N];
+bool dparr[M];
+int cur[M];
 int arr[N];
+
+bool cmp(int one, int two)
+{
+    return one > two;
+}
 
 int main()
 {
@@ -22,31 +34,28 @@ int main()
     std::cin >> num >> kind;
     for (int i = 1; i <= kind; i++)
     {
-        std::cin >> arr[i];
+        scanf("%d", &arr[i]);
     }
-    dparr[0][0] = true;
+    std::sort(arr + 1, arr + 1 + kind, cmp);
+    dparr[0] = true;
+    cur[0] = 0;
     for (int i = 1;; i++)
     {
-        bool flag = false;
-        for (int j = 1; j <= num; j++)
+        cur[i] = num + 1;
+        for (int j = 1; j <= kind; j++)
         {
-            for (int k = 1; k <= kind; k++)
+            if (arr[j] <= i && dparr[i - arr[j]])
             {
-                if (i >= arr[k])
-                {
-                    dparr[i][j] |= dparr[i - arr[k]][j - 1];
-                }
+                dparr[i] = true;
+                cur[i] = std::min(cur[i], cur[i - arr[j]] + 1);
             }
         }
-        for (int j = 1; j <= num; j++)
-        {
-            flag |= dparr[i][j];
-        }
-        if (!flag)
+        if (!dparr[i] || cur[i] > num)
         {
             ans = i - 1;
             break;
         }
+        // std::cout << dparr[i] << " " << cur[i] << std::endl;
     }
     std::cout << ans;
     return 0;
